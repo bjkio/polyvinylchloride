48d0821b6b1fc1553fec9d5b885cbf40dec093dc
Julian <jcksn@bmlr.me.uk>
2020-08-29 14:12:22 +0200

    [pvc-001] Import generated UI components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiHeaderComponent } from './ui-header/ui-header.component';
+import { UiHmenuComponent } from './ui-hmenu/ui-hmenu.component';
+import { UiSidebarComponent } from './ui-sidebar/ui-sidebar.component';
+import { UiContentAreaComponent } from './ui-content-area/ui-content-area.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UiHeaderComponent, UiHmenuComponent, UiSidebarComponent, UiContentAreaComponent],
   imports: [BrowserModule, ClarityModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
